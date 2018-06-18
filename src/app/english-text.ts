292168eb6155a7403e5fcaf770a124a71d23ed73
unknown <lefteris4g@gmail.com>
2018-06-18 13:56:01 +0300

    gg

---
+++ src/app/english-text.ts
@@ -0,0 +1,19 @@
+export const ENGLISH = {
+  companyProfile: 'ABOUT US',
+  // tslint:disable-next-line:max-line-length
+  companyProfileText: 'The company "PLASTIKA-CHAROULI" is active in the field of plastic production and can now look back on a 29-year company history. In all these years, we have specialized exclusively in the production of plastic packaging for fruits and vegetables. The company philosophy and the professionalism of the owner of the company DIAMANTO G. EVANGELOPOULOU, together with the highly qualified employees, form the basis for the increasing recognition in the domestic and foreign markets over the years of operation.',
+  foundation: 'INFRASTRUCTURE',
+  // tslint:disable-next-line:max-line-length
+  foundationText: 'Our company is located in the Makrochori region in the Greek prefecture of Imatheia and its own buildings cover an area of more than 3000 m2. Today our company employs more than 30 permanent employees in production. Constant investments in the modernization of our production facilities and the principle of using high-quality raw materials in conjunction with our experienced staff and above all our respect for new and old partners, guarantee a highly professional and long-term cooperation.',
+  quality: 'AWARDS FOR QUALITY',
+  // tslint:disable-next-line:max-line-length
+  qualityText: 'One of the most significant moments in the company`s history has been the awarding of product quality awards at trade fairs in France and Spain, where we have won over more than 300 companies around the world.',
+  partnership: 'PARTNERSHIP OFFER',
+  // tslint:disable-next-line:max-line-length
+  partnershipText: 'In the certainty of having the infrastructure to meet the challenges of the modern market, we are ready to expand our partnerships in the European market.',
+  contact: 'CONTACT',
+  telephone: 'Telephone',
+  fax: 'Fax',
+  mail: 'Information',
+  mail2: 'Sales department'
+};
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GREEK } from '../greek-text';
+import { ENGLISH } from '../english-text';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  content = GREEK;
 
 
 
@@ -13,5 +15,15 @@ export class MainComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  changeLanguage(language: String) {
+    if (language === 'gr') {
+      this.content = GREEK;
+    } else {
+      this.content = ENGLISH;
+    }
+
+  }
+
 
 }
